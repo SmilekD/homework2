21da54a43c2da69a4de8d96d304351fd73b969ee
David Smilek <smilek.david13@seznam.cz>
2024-01-23 20:54:55 +0100

    Hotový druhý domácí úkol

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String name = "Karel Novotný";
+        LocalDate dateOfBirth = LocalDate.of(1990, 5, 20);
+        int contracts = 2;
+        double soldCarrotsInTons = 5.2;
+        String cityName = "Pardubice";
+        String spz = "7P53425";
+        double fuelEffiency = 6.2;
+        String ipAdress = "192.168.48.39";
+        double averageAmmountOfSoldCarrotPerContract = soldCarrotsInTons / contracts;
+        System.out.println("Průměrné množství prodané mrkve na jednu smlouvu je: " +averageAmmountOfSoldCarrotPerContract+ " tuny.");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+
     }
 }
